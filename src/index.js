b09788d702cd10656343316387552ba1f1e76b5b
Swetarun <s.singh_279@rediffmail.com>
2022-05-17 17:40:12 +0530

    Initial work

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const route = require('./route/route')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+mongoose.connect("mongodb+srv://Swetarun:lBf6gTedHw2tfPtQ@cluster0.ebg8a.mongodb.net/group56Database", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch( err => console.log(err))
+
+app.use('/', route)
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
